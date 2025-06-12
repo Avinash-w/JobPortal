4a8106f82e90db928beac3f8eb5e8cf190ae3707
Avinash <sibbusinghavinash672@gmail.com>
2025-06-12 16:09:22 +0530

    WIP: local changes

--- .next/static/chunks/[root-of-the-server]__2908c8f9._.js
+++ .next/static/chunks/[root-of-the-server]__2908c8f9._.js
@@ -1076,7 +1076,7 @@ if (typeof globalThis.$RefreshHelpers$ === 'object' && globalThis.$RefreshHelper
 
 var { g: global, __dirname, m: module, e: exports } = __turbopack_context__;
 {
-const PAGE_PATH = "/dashboard";
+const PAGE_PATH = "/";
 (window.__NEXT_P = window.__NEXT_P || []).push([
     PAGE_PATH,
     ()=>{
